e78274dd9c565e76874119be397471a36f0fc7b4
jarl sondre sæther <jarlsaether@jarls-MacBook-Pro.local>
2022-06-23 14:44:45 +0200

    add arithmetic

--- src/main/kotlin/no/nav/QuizApplication.kt
+++ src/main/kotlin/no/nav/QuizApplication.kt
@@ -16,6 +16,8 @@ class QuizApplication(private val teamName: String, database: Database? = null):
     override fun handle(question: Question) {
         logger.log(question)
         if (question.category == "team-registration") handleRegisterTeam(question)
+        if (question.category == "arithmetic") handleArithmetic(question)
+
     }
 
 
@@ -26,6 +28,25 @@ class QuizApplication(private val teamName: String, database: Database? = null):
     /**
      * Spørsmål handlers
      */
+
+    private fun operatorFromChar(charOperator: Char):(Int, Int)->Int
+    {
+        return when(charOperator)
+        {
+            '+'->{a,b->a+b}
+            '-'->{a,b->a-b}
+            '/'->{a,b->a/b}
+            '*'->{a,b->a*b}
+            else -> throw Exception("That's not a supported operator")
+        }
+    }
+
+    private fun handleArithmetic(question: Question) {
+        var lists = question.question.split(" ")
+        var myAnswer = operatorFromChar(lists[1].toCharArray()[0]).invoke(lists[0].toInt(), lists[2].toInt())
+        answer(question.category, question.messageId, myAnswer.toString())
+    }
+
 
     private fun handleRegisterTeam(question: Question) {
         logger.log(question)
